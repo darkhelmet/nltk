2940ab43e0188cf4a1f55d9c390af7f78ca7d731
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2012-12-16 22:16:37 -0700

    Stemmer and metaphone stuff

---
+++ filter/double_metaphone.go
@@ -0,0 +1,26 @@
+package filter
+
+/*
+#cgo CFLAGS: -Wno-parentheses
+#include "double_metaphone.h"
+*/
+import "C"
+
+import (
+    "github.com/darkhelmet/nltk"
+    "unsafe"
+)
+
+func DoubleMetaphone(in nltk.TokenChan) nltk.TokenChan {
+    return start(in, func(tok nltk.Token, out nltk.TokenChan) {
+        cs := C.CString(tok.String())
+        defer C.free(unsafe.Pointer(cs))
+        codes := C.double_metaphone(cs)
+        primary, secondary := C.GoString(codes.primary), C.GoString(codes.secondary)
+        defer C.free_dm_result(codes)
+        out <- nltk.Token(primary)
+        if primary != secondary {
+            out <- nltk.Token(secondary)
+        }
+    })
+}
---
+++ filter/stemmer.go
@@ -0,0 +1,26 @@
+package filter
+
+import (
+    "github.com/darkhelmet/nltk"
+    "github.com/darkhelmet/nltk/stemmer"
+)
+
+func stemmerFilter(in nltk.TokenChan, s stemmer.Stemmer) nltk.TokenChan {
+    return start(in, func(tok nltk.Token, out nltk.TokenChan) {
+        out <- nltk.Token(s.Stem(tok.String()))
+    })
+}
+
+func PorterStemmer(in nltk.TokenChan) nltk.TokenChan {
+    s := stemmer.NewPorterStemmer()
+    return stemmerFilter(in, s)
+}
+
+func SnowballStemmer(in nltk.TokenChan) nltk.TokenChan {
+    s, err := stemmer.NewSnowball()
+    if err != nil {
+        // Ugh
+        panic(err)
+    }
+    return stemmerFilter(in, s)
+}
